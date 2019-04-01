19bd3f937bf9e4e8f1ceda72ca5cc5beb4ae9dc5
David Schreck <david.schreck@move.com>
2019-04-01 11:57:53 -0700

    whitespace changes

--- javascript/01_insert_select_bubble_sort.js
+++ javascript/01_insert_select_bubble_sort.js
@@ -1,48 +1,48 @@
 function insertSort(unsorted) {
-    const n = unsorted.length;
-    const sorted = Array(n);
-    
-    sorted[0] = unsorted[0];
+  const n = unsorted.length;
+  const sorted = Array(n);
 
-    let selected;
+  sorted[0] = unsorted[0];
 
-    for (let i = 1; i < n; i++) {        
-        selected = unsorted[i];        
-        let j = i;
-        
-        while (j > 0 && unsorted[j - 1] > selected) {            
-            sorted[j] = sorted[j - 1];            
-            j--;
-        }
-        sorted[j] = selected;        
+  let selected;
+
+  for (let i = 1; i < n; i++) {
+    selected = unsorted[i];
+    let j = i;
+
+    while (j > 0 && unsorted[j - 1] > selected) {
+      sorted[j] = sorted[j - 1];
+      j--;
     }
-    
-    return sorted;
+    sorted[j] = selected;
+  }
+
+  return sorted;
 }
 
 function selectSort(unsorted) {
-    const sorted = [];
-    const n = unsorted.length;
+  const sorted = [];
+  const n = unsorted.length;
 
-    const swap = (input, i, j) => {
-        const tmp = input[i];
-        input[i] = input[j];
-        input[j] = tmp;
-        
-        return input;
-    }
+  const swap = (input, i, j) => {
+    const tmp = input[i];
+    input[i] = input[j];
+    input[j] = tmp;
 
-    for (let i = 0; i < n; i++) {
-        
-    }
+    return input;
+  }
+
+  for (let i = 0; i < n; i++) {
+
+  }
 }
 
 function bubbleSort(input) {
-    //
+  //
 }
 
 module.exports = {
-    insertSort,
-    selectSort,
-    bubbleSort
+  insertSort,
+  selectSort,
+  bubbleSort
 }
